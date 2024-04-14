c40b8f6e908b3ff76cf33a830b5ba82cda09187d
Chris Festus Otopa Ayeh-Datey <chrisaydat@gmail.com>
2024-04-14 15:44:23 +0000

    did some log out stuff

--- lib/student/student_attendance.dart
+++ lib/student/student_attendance.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: prefer_const_constructors, use_key_in_widget_constructors, use_build_context_synchronously, prefer_const_constructors_in_immutables, unused_import, unused_element
+// ignore_for_file: prefer_const_constructors, use_key_in_widget_constructors, use_build_context_synchronously, prefer_const_constructors_in_immutables, unused_import, unused_element, avoid_print
 
 import 'package:attendance_app/components/students/student_bottom_nav_bar.dart';
 import 'package:attendance_app/helpers/database_helper.dart';
